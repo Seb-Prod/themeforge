c8bc618ae44b3e62135e5d8bdc9b23a5e2470462
Seb-Prod <sebastien.drillaud@gmail.com>
2026-08-02 10:42:44 +0200

    feat: generate component css mappings

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,6 @@
 import {
   createTheme,
+  generateComponentCss,
   generateComponentTokens,
   generateCssVariables,
   generateScale,
@@ -40,11 +41,13 @@ export default function Home() {
 
   const css = generateCssVariables(theme);
   const components = generateComponentTokens(theme);
+  const css2 = generateComponentCss(theme);
 
   // console.log(theme);
   // console.log(generateCssVariables(theme));
-
   console.log(components);
+
+  console.log(css2.join("\n"));
 
   return (
     <div>
--- packages/color-engine/src/component/index.ts
+++ packages/color-engine/src/component/index.ts
@@ -1,2 +1,3 @@
 export * from "./types";
 export * from "./generate-component-tokens";
+export * from "./generate-component-css";
